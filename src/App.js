bdbc5598f6bbb19f98b60c2e0b5d155fb0230ccc
NAMBIRAJA M <nambirajam.ug.21.cs@scadengineering.ac.in>
2024-12-23 18:18:52 +0530

    slight changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Dashboard from './components/Dashboard';
-/* import Login from './components/Login';
- */
+/* import Login from './components/Login'; */
+
 function App() {
   return (
     <div className="App">
